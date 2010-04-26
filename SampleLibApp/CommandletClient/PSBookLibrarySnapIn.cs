5c32b68730991d9dfe59a7627ec9778f4df6bb0b
Rasika Weliwita <weliwita@gmail.com>
2010-04-26 09:54:38 +0530

    Added PSSnapin

---
+++ SampleLibApp/CommandletClient/PSBookLibrarySnapIn.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Management.Automation;
+using System.ComponentModel;
+
+namespace CommandletClient
+{
+    [RunInstaller(true)]
+    public class PSBookLibrarySnapIn : PSSnapIn
+    {
+
+        public override string Description
+        {
+            get { return "This Windows PowerShell snap-in contains methods for manipulating books in CSLLibrary"; }
+        }
+
+        public override string Name
+        {
+            get { return "CST.BookLibrary.PSClient"; }
+        }
+
+        public override string Vendor
+        {
+            get { return "CST"; }
+        }
+    }
+}
